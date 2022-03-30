eab794c307fd8ab16c5678a9ff62c5b63bc18ca5
Diogenes Konorath <diogenesks@gmail.com>
2022-03-30 20:21:49 -0300

    add new module

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
 terraform {
- backend "remote" {
+  backend "remote" {
     organization = "ilg-mentoria-diogenes"
 
     workspaces {
       name = "aws-enviroment"
     }
   }
+}
+
+module "client-vpc"{
+  source       ="git@github.com:diogeneskonorath/aws-vpc.git"
+  vpc_cidr     = var.vpc_cidr
+  vpc_name     = var.environment
+  nat_count    = 1
+  cluster_name = var.cluster_name
+  environment  = var.environment
+  aws_region   = var.aws_region
 }
